544e41cf794c99a8ac5a439aacd845f1d6e6ec5e
Alex Muravya <kyberorg@yadev.eu>
2020-08-12 10:19:35 +0300

    Component: props and usage

--- webapp/src/app/components/car/car.component.ts
+++ webapp/src/app/components/car/car.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-car',
-  templateUrl: './car.component.html',
-  styleUrls: ['./car.component.css']
+    selector: 'app-car',
+    templateUrl: './car.component.html',
+    styleUrls: ['./car.component.css']
 })
 export class CarComponent implements OnInit {
 
-  constructor() { }
+    name: string;
+    speed: number;
+    model: string;
+    colors: Colors;
+    engine: {
+        volume: number,
+        fuel: string,
+    };
+    options: string[];
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
 
+    ngOnInit(): void {
+        this.name = 'UAZ';
+        this.speed = 90;
+        this.model = 'Patriot';
+        this.colors = {
+            car: 'Orange',
+            salon: 'Grey',
+            wheels: 'Black'
+        };
+        this.engine = {
+            volume: 2.7,
+            fuel: 'bensin'
+        };
+        this.options = ['ABS', '4WD'];
+    }
+}
+
+interface Colors {
+    car: string;
+    salon: string;
+    wheels: string;
 }
